f089408b9a5deb90e2c897fe278190ffa1b21f2f
jasminatarmasheva786 <jasminatarmasheva786@gmail.com>
2023-11-26 23:18:03 +0500

    Finished

--- misc.py
+++ misc.py
@@ -12,7 +12,11 @@ class Misc(Randommer):
         Returns:
             list: list of cultures
         '''
-        pass
+        headers={"X-Api-Key":api_key}
+        url=self.get_url()+"Misc/Cultures"
+        response=requests.get(url,headers=headers)
+        return response.json()
+        
     
     def get_random_address(self, api_key: str, number: int, culture='en') -> list:
         '''get available misc cultures
@@ -25,4 +29,19 @@ class Misc(Randommer):
         Returns:
             list: random address
         '''
-        pass
+        headers={
+            "X-Api-Key":api_key
+        }
+        params={
+            "number":number,
+            "culture":culture
+        }
+
+        url=self.get_url()+"Misc/Random-Address"
+        resp=requests.get(url,headers=headers,params=params)
+
+        return resp.json()
+
+token="cada6da7cd4e41238712196daafaa11e"
+v=Misc()
+print(v.get_random_address(api_key=token,number=1))
